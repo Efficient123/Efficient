a9284038fd3c88de2b3ad10eb8b6a02b35f34443
Efficient123 <efficient0123@gmail.com>
2024-06-04 09:00:54 +0530

    Update death.js

--- commands/utility/death.js
+++ commands/utility/death.js
@@ -20,7 +20,7 @@ module.exports = {
 		const offender = i.options.getUser("criminal");
 		const reason = i.options.getString("reason") ?? "No reason provided";
     const executer = i.member;
-		if (!executer.roles.cache.has("1193792031751934002") || executer.roles.cache.has("1193771724886511707") || executer.roles.cache.has("1239071556576088094") || executer.roles.cache.has("1193509108364623882")) {
+		if (!(executer.roles.cache.has("1193792031751934002") || executer.roles.cache.has("1193771724886511707") || executer.roles.cache.has("1239071556576088094") || executer.roles.cache.has("1193509108364623882"))) {
 			await i.reply("You do not have permission to use this command.");
 		} else {
 			await i.reply(`Sentenced ${offender} to death for ${reason}`);
