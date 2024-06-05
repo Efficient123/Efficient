f6082935bd857292ee38f80e65b2f78139c0539a
Efficient123 <efficient0123@gmail.com>
2024-06-05 09:26:46 +0530

    Update death.js

--- commands/utility/death.js
+++ commands/utility/death.js
@@ -3,38 +3,50 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('death_sentence')
-		.setDescription('sentences a user to death\(ban\)')
-	  .addUserOption((option) =>
+		.setDescription('Sentences a user to death (ban)')
+		.addUserOption((option) =>
 			option
 				.setName("criminal")
 				.setDescription("The user to be banned")
-				.setRequired(true),
+				.setRequired(true)
 		)
-	  .addStringOption((option) =>
+		.addStringOption((option) =>
 			option
 				.setName("reason")
 				.setDescription("Reason for the ban")
-				.setRequired(false),
+				.setRequired(false)
 		),
 	async execute(i) {
-		const offender = i.options.getUser("criminal");
+		const offender = i.options.getMember("criminal"); // Get the GuildMember instead of User
 		const reason = i.options.getString("reason") ?? "No reason provided";
-    const executer = i.member;
-		const execuse = ["Efficient's anti moderation sheild was too hard to penetrate", "Efficient, look someone is trying to execute you, your majesty 🙏", "I will execute you instead", "Uno", "They are too important, unlike you."]
+		const executer = i.member;
+		const execuse = [
+			"Efficient's anti-moderation shield was too hard to penetrate",
+			"Efficient, look someone is trying to execute you, your majesty 🙏",
+			"I will execute you instead",
+			"Uno",
+			"They are too important, unlike you."
+		];
+
 		if (!(executer.roles.cache.has("1193792031751934002") || executer.roles.cache.has("1193771724886511707") || executer.roles.cache.has("1239071556576088094") || executer.roles.cache.has("1193509108364623882"))) {
 			await i.reply("You do not have permission to use this command.");
-		} else if (offender.roles.cache.has("1241383081617915925") || offender.roles.cache.has("1193834032023216168")){
-			await i.reply("I cannot execute them.")
+		} else if (offender.roles.cache.has("1241383081617915925") || offender.roles.cache.has("1193834032023216168")) {
+			await i.reply("I cannot execute them.");
 		} else if (offender.roles.cache.has("1193509108364623882")) {
-			await i.reply(`${execuse[Math.floor(Math.random() * execuse.length)]}`)
+			await i.reply(`${execuse[Math.floor(Math.random() * execuse.length)]}`);
 		} else {
-			await i.reply(`Sentenced ${offender} to death for ${reason}`);
+			await i.reply(`Sentenced ${offender.user.tag} to death for ${reason}`); // Use offender.user.tag for username
 			try {
-				offender.send(`You have been sentenced to death for ${reason}`);
-			} catch(e) {
-        console.log(e)
+				await offender.send(`You have been sentenced to death for ${reason}`);
+			} catch (e) {
+				console.log(e);
 			}
-			await i.guild.members.ban(offender, { reason: reason });
+			try {
+				await i.guild.members.ban(offender, { reason: reason });
+			} catch (e) {
+				console.log(e);
+				await i.followUp("There was an error while trying to ban the user.");
+			}
 		}
 	},
 };
