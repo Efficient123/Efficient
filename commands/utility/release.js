dfc4b5a6b462a6a1f29e297ad5999a5fc620f666
Efficient123 <efficient0123@gmail.com>
2024-06-05 19:19:06 +0530

    Update release.js

--- commands/utility/release.js
+++ commands/utility/release.js
@@ -1,8 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('releass')
+		.setName('release')
 		.setDescription('Releases a user.')
 		.addUserOption((option) =>
 			option
@@ -13,26 +15,63 @@ module.exports = {
 	async execute(i) {
 		const offender = i.options.getMember("criminal"); // Get the GuildMember instead of User
 		const executer = i.member;
-		const execuse = [
-			"Efficient's anti-moderation shield was too hard to penetrate",
-			"Efficient, look someone is trying to execute you, your majesty 🙏",
-			"I will execute you instead",
-			"Uno",
-			"They are too important, unlike you."
-		];
 
 		if (!(executer.roles.cache.has("1193792031751934002") || executer.roles.cache.has("1193771724886511707") || executer.roles.cache.has("1239071556576088094") || executer.roles.cache.has("1193509108364623882"))) {
 			await i.reply("You do not have permission to use this command.");
-		} else if (!(offender.roles.cache.has("1243049250447949905"))) {
+			return;
+		} 
+
+		if (!offender.roles.cache.has("1243049250447949905")) {
 			await i.reply("They are not in jail.");
-		} else {
-			await i.reply(`Sentenced ${offender.user.tag} to death for ${reason}`); // Use offender.user.tag for username
-			try {
-				await offender.send(`You have been released.`);
-				offender.roles.cache.remove("1243049250447949905")
-			} catch (e) {
-				console.log(e);
-			}
+			return;
 		}
+
+		const punishFilePath = path.join(__dirname, "punish.json");
+
+		fs.readFile(punishFilePath, "utf8", async (err, data) => {
+			if (err) {
+				console.error("Error reading punish.json:", err);
+				await i.reply("There was an error accessing the punishment records.");
+				return;
+			}
+
+			let punishments = JSON.parse(data);
+			const punishmentIndex = punishments.findIndex(p => p.id === offender.id);
+
+			if (punishmentIndex === -1) {
+				await i.reply("No record of this user in punish.json.");
+				return;
+			}
+
+			const punishment = punishments[punishmentIndex];
+
+			try {
+				const cellChannel = i.guild.channels.cache.get(punishment.cell);
+				if (cellChannel) {
+					await cellChannel.delete();
+				}
+			} catch (e) {
+				console.error("Error deleting cell channel:", e);
+			}
+
+			punishments.splice(punishmentIndex, 1);
+
+			fs.writeFile(punishFilePath, JSON.stringify(punishments, null, 2), "utf8", async (err) => {
+				if (err) {
+					console.error("Error writing to punish.json:", err);
+					await i.reply("There was an error updating the punishment records.");
+					return;
+				}
+
+				try {
+					await offender.roles.remove("1243049250447949905");
+					await offender.send(`You have been released.`);
+					await i.reply(`Released ${offender.user.tag} from jail.`);
+				} catch (e) {
+					console.error("Error removing jail role or sending message:", e);
+					await i.reply("There was an error releasing the user.");
+				}
+			});
+		});
 	},
 };
