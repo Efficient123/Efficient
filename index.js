d787436afaefa931f85ddcfbf328317c4c1c136c
Efficient123 <efficient0123@gmail.com>
2024-06-05 17:31:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token, guildId } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+	],
+});
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
@@ -25,6 +29,8 @@ for (const folder of commandFolders) {
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	checkPunishments();
+	setInterval(checkPunishments, 50 * 1000); // Check every 5 minutes
 });
 
 client.on(Events.InteractionCreate, async interaction => {
@@ -47,5 +53,47 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
+
+const punishFilePath = path.join(__dirname, 'commands', 'utility', 'punish.json');
+
+async function checkPunishments() {
+	try {
+		const data = fs.readFileSync(punishFilePath, 'utf8');
+		const punishments = JSON.parse(data);
+		const currentDate = new Date();
+
+		for (let i = punishments.length - 1; i >= 0; i--) {
+			const punishment = punishments[i];
+			if (punishment.time !== 'life' && new Date(punishment.time) <= currentDate) {
+				const guild = await client.guilds.fetch(guildId);
+				const member = await guild.members.fetch(punishment.id);
+				const channel = guild.channels.cache.get(punishment.cell);
+
+				if (member) {
+					await member.roles.remove('1243049250447949905'); // Replace with your jailed role ID
+					try {
+						await member.send(`You have been released from jail.`);
+					} catch (err) {
+						console.log(`Could not send a DM to ${member.user.tag}.`);
+					}
+					console.log(`Released ${member.user.tag} from jail.`);
+				}
+
+				if (channel) {
+					await channel.delete();
+					console.log(`Deleted channel ${channel.name}`);
+				}
+
+				// Remove the punishment from the array
+				punishments.splice(i, 1);
+			}
+		}
+
+		// Write the updated punishments array back to the file
+		fs.writeFileSync(punishFilePath, JSON.stringify(punishments, null, 2), 'utf8');
+	} catch (error) {
+		console.error('Error checking punishments:', error);
+	}
+}
 
 client.login(token);
